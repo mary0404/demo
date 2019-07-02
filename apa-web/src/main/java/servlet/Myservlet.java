eb7f7712fb5af7764d5a6c10339f84d545dc1ac5
Administrator <2720534371@qq.com>
2019-07-02 14:58:55 +0800

    6

---
+++ apa-web/src/main/java/servlet/Myservlet.java
@@ -0,0 +1,34 @@
+package servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class Myservlet implements javax.servlet.Servlet{
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+    // int;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
